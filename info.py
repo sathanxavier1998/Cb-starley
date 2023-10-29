3003de5d74ac3bdeb2cd2a03905715f97a3fced5
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-10-29 21:50:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '5569911992:AAEz9VF_xYlMXR0DdnvSon7__GUgRjO
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
-NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/62cec29b0669206b7943a.jpg")
+NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/5e0c5141f5e6e8cd600c2.jpg")
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/f83017d890b1b692a673a.jpg https://te.legra.ph/file/72602fb1f87727dde571b.jpg https://te.legra.ph/file/e37fbea2190470aaf4bc1.jpg https://te.legra.ph/file/9f4f70db519f1bdbcb27b.jpg https://te.legra.ph/file/72d74f632a2317f21136b.jpg https://te.legra.ph/file/018e5893fdaabe0f1cef4.jpg')).split()
 SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/314928baed9a9c277072f.jpg")
 
